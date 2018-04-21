cb6c0141f12789d53f8c106504a00d338ae4d5b0
ErickRyu <ErickRyu>
2018-04-21 16:03:50 +0900

    Refactoring: Extract method regularMovieTest

--- src/main/java/step1/CustomerTest.java
+++ src/main/java/step1/CustomerTest.java
@@ -17,22 +17,25 @@ public class CustomerTest {
     @Test
     public void testY(){
         int daysRented = 0;
-        String name = null;
-        Customer customer = new Customer(name);
-        int priceCode = 0;
-        String title = null;
-        Movie movie = new Movie(title, priceCode);
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        Customer customer = createRental(daysRented);
         assertEquals("Rental Record for null\n" +
                 "\t2.0(null)\n" +
                 "Amount owed is 2.0\n" +
                 "You earned 1 frequent renter pointers", customer.statement());
     }
 
+
     @Test
     public void regularMovieTest(){
         int daysRented = 3;
+        Customer customer = createRental(daysRented);
+        assertEquals("Rental Record for null\n" +
+                "\t3.5(null)\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter pointers", customer.statement());
+    }
+
+    private Customer createRental(int daysRented) {
         String name = null;
         Customer customer = new Customer(name);
         int priceCode = 0;
@@ -40,10 +43,7 @@ public class CustomerTest {
         Movie movie = new Movie(title, priceCode);
         Rental rental = new Rental(movie, daysRented);
         customer.addRental(rental);
-        assertEquals("Rental Record for null\n" +
-                "\t3.5(null)\n" +
-                "Amount owed is 3.5\n" +
-                "You earned 1 frequent renter pointers", customer.statement());
+        return customer;
     }
 
 }
