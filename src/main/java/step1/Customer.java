adea67d5146d02be4c7ff7a5aca091a4eb61c466
ErickRyu <ErickRyu>
2018-04-22 17:35:10 +0900

    Refact: Replace frequentRenterPoints tmp with query

--- src/main/java/step1/Customer.java
+++ src/main/java/step1/Customer.java
@@ -1,10 +1,8 @@
 package step1;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 public class Customer {
 	private String name;
@@ -14,8 +12,6 @@ public class Customer {
 		this.name = name;
 	}
 
-	;
-
 	public void addRental(Rental rental) {
 		rentals.add(rental);
 	}
@@ -24,26 +20,19 @@ public class Customer {
 		return name;
 	}
 
-	;
-
 	public String statement() {
-		int frequentRenterPoints = 0;
 		Iterator<Rental> iterator = rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 
 		while (iterator.hasNext()) {
 			Rental each = (Rental) iterator.next();
-			// determine amounts for each line
 
-			// add frequent renter points
-			frequentRenterPoints = each.getFrequentRenterPoints();
 			// show figures
 			result += "\t" + String.valueOf(each.getCharge()) + "(" + each.getMovie().getTitle() + ")" + "\n";
-
 		}
 
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter pointers";
+		result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter pointers";
 
 		return result;
 	}
@@ -58,4 +47,13 @@ public class Customer {
 		return result;
 	}
 
+	private int getFrequentRenterPoints(){
+		int result = 0;
+		Iterator<Rental> iterator = rentals.iterator();
+		while (iterator.hasNext()) {
+			Rental each = (Rental) iterator.next();
+			result = each.getFrequentRenterPoints();
+		}
+		return result;
+	}
 }
