7dd69110c4189d230060fea2221939198293d1df
ErickRyu <ErickRyu>
2018-04-22 16:33:25 +0900

    Refact: Extract method getFrequentRenterPoint

--- src/main/java/step1/Customer.java
+++ src/main/java/step1/Customer.java
@@ -34,10 +34,7 @@ public class Customer {
 			// determine amounts for each line
 
 			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = each.getFrequentRenterPoints();
 			// show figures
 			result += "\t" +  String.valueOf(each.getCharge()) + "(" + each.getMovie().getTitle() + ")" + "\n";
 
@@ -49,5 +46,6 @@ public class Customer {
 
 		return result;
 	}
+
 
 }
