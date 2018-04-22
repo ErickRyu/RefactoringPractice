0b9df901246d19a4013d9d8e4197517f612a7d4c
ErickRyu <ErickRyu>
2018-04-22 15:29:08 +0900

    Create: children movie test more than three days

--- src/main/java/step1/CustomerTest.java
+++ src/main/java/step1/CustomerTest.java
@@ -7,6 +7,7 @@ public class CustomerTest {
 
     public static final int TWO_DAYS = 2;
     public static final int THREE_DAYS = 3;
+    public static final int FOUR_DAYS = 4;
     String name = null;
     Customer customer = new Customer(name);
 
@@ -46,6 +47,26 @@ public class CustomerTest {
                 "\t9.0(null)\n" +
                 "Amount owed is 9.0\n" +
                 "You earned 2 frequent renter pointers", customer.statement());
+    }
+
+    @Test
+    public void childrenMovie_in_statement_less_than_four_days(){
+        Rental rental = createRental(THREE_DAYS, Movie.CHILDRENS);
+        customer.addRental(rental);
+        assertEquals("Rental Record for null\n" +
+                "\t1.5(null)\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter pointers", customer.statement());
+    }
+
+    @Test
+    public void childrenMovie_in_statement_more_than_three_days(){
+        Rental rental = createRental(FOUR_DAYS, Movie.CHILDRENS);
+        customer.addRental(rental);
+        assertEquals("Rental Record for null\n" +
+                "\t3.0(null)\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter pointers", customer.statement());
     }
 
     private Rental createRental(int daysRented, int priceCode) {
